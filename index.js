bfd758c73674413714641c7d8659bf8896f4055c
Tim Lewis <timothy.james.lewis.31@gmail.com>
2024-03-29 01:04:00 -0500

    everything seems to be working just need to add comments and the test

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const {Circle, Triangle, Square} = require('./lib/shapes')
 
 const questions = [
     {
         type: 'input',
         message: 'Which letters would you like in your logo? (1-3 letters)',
-        name: 'letters'
+        name: 'letters',
+        validate: function(input) {
+            if (input.length > 3 || input.length === 0) {
+                return 'Please enter 1 to 3 letters'
+            }
+            return true;
+        }
     },
     {
         type: 'input',
@@ -23,4 +30,37 @@ const questions = [
         message: 'What color would you like the shape to be? (you can use a color keyword or a hexadecimal)',
         name: 'shapeColor'
     },
-]
+];
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log('Generated logo.svg'))
+};
+
+
+function generateSVG(data) {
+    let chosenShape;
+    switch(data.shape) {
+        case 'Circle':
+            chosenShape = new Circle(data.shapeColor, data.letters, data.letterColor);
+            break;
+        case 'Triangle':
+            chosenShape = new Triangle(data.shapeColor, data.letters, data.letterColor);
+            break;
+        case 'Square':
+            chosenShape = new Square(data.shapeColor, data.letters, data.letterColor);
+            break;
+    }
+    return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+                ${chosenShape.render()}
+                ${chosenShape.renderLetters()}
+            </svg>`;
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => writeToFile('logo.svg', generateSVG(data)))
+;}
+
+init();
